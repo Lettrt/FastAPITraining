56b3cf564be6f5cc5aff798f9e474bfe00d73ac8
Alex <ak555110689@gmail.com>
2023-10-22 01:21:43 +0600

    validates and classes

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from datetime import datetime
+from enum import Enum
 
 app = FastAPI(
     title='Trading App'
@@ -8,11 +12,28 @@ fake_users = [
     {'id': 1, 'role': 'admin', 'name': 'Bob'},
     {'id': 2, 'role': 'investor', 'name': 'John'},
     {'id': 3, 'role': 'trader', 'name': 'Matt'},
+    {'id': 4, 'role': 'trader', 'name': 'Homer', 'degree': [
+        {'id': 1, 'created_at': '2020-01-01T00:00:00', 'type_degree': 'expert'}
+        ]},
 ]
 
+class DegreeType(Enum):
+    newbie = 'newbie'
+    exper = 'expert'
 
 
-@app.get('/users/{user_id}')
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+class User(BaseModel):
+    id: int
+    role: str
+    name : str
+    degree : Optional[List[Degree]] = []
+
+@app.get('/users/{user_id}', response_model=List[User])
 def get_user(user_id: int):
     return [user for user in fake_users if user.get('id') == user_id]
 
@@ -21,9 +42,9 @@ fake_trades = [
     {'id': 2, 'user_id': 1, 'currency': 'BTC', 'side': 'sell', 'price': 125, 'ammount': 2.12}
 ]
 
-@app.get('/trades')
-def get_trades(limit: int = 1, offset: int = 0):
-    return fake_trades[offset:][:limit]
+# @app.get('/trades')
+# def get_trades(limit: int = 1, offset: int = 0):
+#     return fake_trades[offset:][:limit]
 
 fake_users2 = [
     {'id': 1, 'role': 'admin', 'name': 'Bob'},
@@ -36,3 +57,17 @@ def change_user_name(user_id:int, new_mame:str):
     current_user = list(filter(lambda user: user.get('id') == user_id, fake_users2))[0]
     current_user['name'] = new_mame
     return {'stauts': 200, 'data': current_user}
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0) # ge означает >=
+    amount: float
+
+
+@app.post('/trades')
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {'status': 200, 'data': fake_trades}
